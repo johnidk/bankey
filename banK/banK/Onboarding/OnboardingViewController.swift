3f47ebb9cef46075983c72c00d4b321fd03a6f58
johnidk <johngabrielbarreto1337@gmail.com>
2023-01-11 18:05:37 -0300

    refactoring and added generic onboarding view controller

--- banK/banK/Onboarding/OnboardingViewController.swift
+++ banK/banK/Onboarding/OnboardingViewController.swift
@@ -2,123 +2,66 @@
 //  OnboardingViewController.swift
 //  banK
 //
-//  Created by João Gabriel Lavareda Ayres Barreto on 09/01/23.
-//
-
-import Foundation
-
-//
-//  OnboardingContainerViewController.swift
-//  Bankey
-//
-//  Created by jrasmusson on 2021-09-28.
+//  Created by João Gabriel Lavareda Ayres Barreto on 11/01/23.
 //
 
 import UIKit
 
-class OnboardingContainerViewController: UIViewController {
-
-    let pageViewController: UIPageViewController
-    var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-        }
+class OnboardingViewController: UIViewController {
+    let stackView = UIStackView()
+    let image = UIImageView()
+    let label = UILabel()
+    
+    let heroImageName: String
+    let titleText: String
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
     }
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    init(heroImageName:String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
         
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
-        
-        pages.append(page1)
-        pages.append(page2)
-        pages.append(page3)
-        
-        currentVC = pages.first!
-        
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension OnboardingViewController {
+    func style() {
+        view.backgroundColor = .systemBackground
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: heroImageName)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = titleText
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = .systemPurple
-        
-        addChild(pageViewController)
-        view.addSubview(pageViewController.view)
-        pageViewController.didMove(toParent: self)
-        
-        pageViewController.dataSource = self
-        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+    func layout() {
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(label)
+        view?.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
-            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
-            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
         ])
-        
-        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
-        currentVC = pages.first!
-    }
-}
-
-// MARK: - UIPageViewControllerDataSource
-extension OnboardingContainerViewController: UIPageViewControllerDataSource {
-
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        return getPreviousViewController(from: viewController)
-    }
-
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return getNextViewController(from: viewController)
-    }
-
-    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
-        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
-        currentVC = pages[index - 1]
-        return pages[index - 1]
-    }
-
-    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
-        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
-        currentVC = pages[index + 1]
-        return pages[index + 1]
-    }
-
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return pages.count
-    }
-
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        return pages.firstIndex(of: self.currentVC) ?? 0
-    }
-}
-
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
     }
 }
