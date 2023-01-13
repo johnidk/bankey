3bdb42d15e5b4d8711d4cec7bbe8f632d288314d
johnidk <johngabrielbarreto1337@gmail.com>
2023-01-13 18:46:00 -0300

    added table view

--- banK/banK/AppDelegate.swift
+++ banK/banK/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         dummyViewController.logoutDelegate = self
         
         
-        window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
 
         return true
     }
